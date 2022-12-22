e278bcc2e45d18792f53e6d7797ccbe368afc5b5
innovitics-inc <72441783+innovitics-inc@users.noreply.github.com>
2022-12-22 13:55:19 +0200

    logging revamp 3

--- src/main/java/innovitics/azimut/utilities/logging/MyLogger.java
+++ src/main/java/innovitics/azimut/utilities/logging/MyLogger.java
@@ -3,10 +3,6 @@ package innovitics.azimut.utilities.logging;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import innovitics.azimut.utilities.threading.DemoTask;
-import innovitics.azimut.utilities.threading.MyThread;
-import innovitics.azimut.utilities.threading.UniqueThreadIdGenerator;
-
 public final class MyLogger {
 	protected static final Logger logger = LoggerFactory.getLogger(MyLogger.class);
 
